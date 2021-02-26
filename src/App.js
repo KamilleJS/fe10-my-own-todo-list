dd4cb628052c1f37addf8661766c0e6092a49b8b
KamilleJS <jkuklinaj@gmail.com>
2021-02-26 15:19:07 -0800

    initial list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+    const initialList = [
+        {id: 1, title: 'learn JS', status: 'undone'},
+        {id: 2, title: 'learn React', status: 'undone'},
+        {id: 3, title: 'find job', status: 'undone'}
+    ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Todo list</h2>
     </div>
   );
 }
